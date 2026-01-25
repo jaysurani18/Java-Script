d8ce6939c0b5dd334f3bed92db8b0d4d142eefd5
jaysurani18 <jaysurani008@gmail.com>
2026-01-25 10:33:17 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,15 @@
+// const date = 1888;
+// date = 2024;
+// console.log(date);
+// remeber const can not be changed
+
+let name = "jay";
+
+let x;
+
+var nouse = "problem in block scope don not use var";
+
+jay = "hello";
+
+
+console.table([name, nouse, jay,x]);
