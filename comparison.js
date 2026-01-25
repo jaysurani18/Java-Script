9a84e6aa5e88d97bb81e86aa3e326fc7da6aff22
jaysurani18 <jaysurani008@gmail.com>
2026-01-25 23:00:50 +0530

    == , ===

---
+++ comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);  // simple equals only checks for value
+console.log(null === 0);// tripal equals checks for type also
+
+
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
