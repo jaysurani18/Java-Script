0661b8b9d61fce6718967eb29e8ea6898977f748
jaysurani18 <jaysurani008@gmail.com>
2026-01-25 10:33:30 +0530

    datatypes

---
+++ 2datatypes.js
@@ -0,0 +1,14 @@
+let name = "jay"
+let height = 6
+let male = true
+let weight = 60
+console.table([name, height, male, weight]);
+
+// let use declare many datatypes
+// string
+// number
+// boolean
+// undefined
+// null
+console.log(typeof null);     // null's type is object
+console.log(typeof undefined);     // undefined's type is undefined
