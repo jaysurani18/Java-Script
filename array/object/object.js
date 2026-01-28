b56711104b672f56581770ff90909eaedf60a71a
jaysurani18 <jaysurani008@gmail.com>
2026-01-29 00:14:15 +0530

    obj

--- array/object/object.js
+++ array/object/object.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "jay",
+    "full name": " jay",
     [mySym]: "mykey1",
     age: 18,
     location: "Jaipur",
-    email: "hitesh@google.com",
+    email: "jay@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "jay@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "jay@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
