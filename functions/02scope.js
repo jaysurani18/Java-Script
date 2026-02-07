fdf7b5b830b52b23666ec1d4c955a4d5c76fda9a
jaysurani18 <jaysurani008@gmail.com>
2026-02-07 23:41:48 +0530

    inside function in block scope

---
+++ functions/02scope.js
@@ -0,0 +1,13 @@
+function one() {
+    const username = "jay"
+    
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    conslole.log(website);
+    two()
+
+}
+
+one()
