f19eb73be60c9470751093c17cdcdcd92c28ab0f
jaysurani18 <jaysurani008@gmail.com>
2026-02-09 23:54:37 +0530

    what is functional scope and block scope

--- functions/02scope.js
+++ functions/02scope.js
@@ -1,3 +1,16 @@
+// block scope {let, const} this two are inside of {} and they can not access outside of the block scope 
+//var is function scope and it can access outside of the function scope
+
+function scope() {
+    let a = "jay"
+    var age = 22
+    const website = "youtube"
+}
+
+console.log(name); // error
+console.log(age); // undefined
+console.log(website); // error
+
 function one() {
     const username = "jay"
     
