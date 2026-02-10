95cfd3bbe2cb9ea6783743d938a1943b31c6d5a4
jaysurani18 <jaysurani008@gmail.com>
2026-02-10 23:45:42 +0530

    add block scope example to demonstrate variable accessibility

--- functions/03scope.js
+++ functions/03scope.js
@@ -13,3 +13,16 @@ function one( ) {
 }
 
 one()
+
+
+// using if checking to show the block scope and function scope
+
+if (true) {
+    const username = "jay";
+    if (username === "jay") {
+        const website = " youtube";
+        console.log(username + website ); // accessible because of closure AND THEY CAN ACCESS GLOBAL SCOPE
+    }
+    console.log(website); // not accessible because it is declared inside of the if block and it is not accessible outside of the if block
+}
+console.log(username); // not accessible because it is declared inside of the if block and it is not accessible outside of the if block
