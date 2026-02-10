10ba47cd5b056c8a4a5707a0dbc0a8740dacb1c8
jaysurani18 <jaysurani008@gmail.com>
2026-02-10 23:56:09 +0530

    refactor: uncomment code to enhance clarity on scope examples

--- functions/03scope.js
+++ functions/03scope.js
@@ -1,46 +1,46 @@
-// function one( ) {                       
-//     const username = "jay";
+function one( ) {                       
+    const username = "jay";
 
-//       function two(){
-//         const website = "youtube";
-//         console.log(username); // accessible because of closure AND THEY CAN ACCESS GLOBAL SCOPE
+      function two(){
+        const website = "youtube";
+        console.log(username); // accessible because of closure AND THEY CAN ACCESS GLOBAL SCOPE
         
-//       }
-//       console.log(website); // not accessible because it is declared inside of the function two and it is not accessible outside of the function two
+      }
+      console.log(website); // not accessible because it is declared inside of the function two and it is not accessible outside of the function two
 
-//       two();
+      two();
     
-// }
+}
 
-// one()
-
-
-// // using if checking to show the block scope and function scope
-
-// if (true) {
-//     const username = "jay";
-//     if (username === "jay") {
-//         const website = " youtube";
-//         console.log(username + website ); // accessible because of closure AND THEY CAN ACCESS GLOBAL SCOPE
-//     }
-//     console.log(website); // not accessible because it is declared inside of the if block and it is not accessible outside of the if block
-// }
-// console.log(username); // not accessible because it is declared inside of the if block and it is not accessible outside of the if block
+one()
 
 
+// using if checking to show the block scope and function scope
 
-// //variable can make a function
-// function addone(num){
-//     return num + 1;
-// }
+if (true) {
+    const username = "jay";
+    if (username === "jay") {
+        const website = " youtube";
+        console.log(username + website ); // accessible because of closure AND THEY CAN ACCESS GLOBAL SCOPE
+    }
+    console.log(website); // not accessible because it is declared inside of the if block and it is not accessible outside of the if block
+}
+console.log(username); // not accessible because it is declared inside of the if block and it is not accessible outside of the if block
+
+
+
+//variable can make a function
+function addone(num){
+    return num + 1;
+}
  
-// addone(5) 
+addone(5) 
 
-// const addtwo = function (num){     // in variable we can declare a function 
-//     return num + 2;
-// }
+const addtwo = function (num){     // in variable we can declare a function 
+    return num + 2;
+}
 
-// addtwo(5)
+addtwo(5)
 
 
 
